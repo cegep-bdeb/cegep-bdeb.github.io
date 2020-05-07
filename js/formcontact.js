55509d9f2e501ff3b753a73ab1feb9a051d7414e
ayachi-65 <64625371+ayachi-65@users.noreply.github.com>
2020-05-06 21:40:47 -0400

    upload
    1 ere upload

---
+++ js/formcontact.js
@@ -0,0 +1,26 @@
+function validate(inputText){
+var name = document.getElementById("name").value;
+var email = document.getElementById("email").value;
+var message = document.getElementById("message").value;
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+if ( email != "" && email != "" && message!="") 
+{ 
+             if(inputText.value.match(mailformat))
+            {
+            alert ("votre requête a été envoyée");
+             alert ("merci de votre contact");
+            document.formcontact.email.focus();
+            return true;
+            }
+            else
+            {
+            alert("Entrer une adresse mail valide !");
+            document.formcontact.email.focus();
+            return false;
+            }
+            }
+else{
+alert("svp remplir tout les chemps!!")
+    return true;
+}
+}
